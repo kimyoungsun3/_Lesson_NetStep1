603d72a5c2ac898652d7d7781f2b1b86ded7ebcb
kimyoungsun3 <40338139+kimyoungsun3@users.noreply.github.com>
2019-02-01 16:37:07 +0900

    4444
    444

---
+++ 09_CSharpTest/EventArgs3_Callback/EventArgs3_Callback/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EventArgs3_Callback
+{
+	class CustomeEventArgs : EventArgs
+	{
+		public int callCount;
+		public string msg;
+		public CustomSocket socket;
+		public object UserToken;
+
+		public event EventHandler<CustomeEventArgs> Complete;
+		public CustomeEventArgs(string _msg)
+		{
+			callCount = 0;
+			msg = _msg;
+		}
+
+		public void OnComplete(object _obj)
+		{
+			if(Complete != null)
+			{
+				callCount++;
+				Complete(_obj, this);
+			}
+		}
+	}
+
+	class CustomSocket
+	{
+		CustomeEventArgs acceptArgs, receiveArgs, sendArgs;
+		public void AcceptAsync(CustomeEventArgs _args)		{ acceptArgs = _args; }
+		public void ReceiveAsync(CustomeEventArgs _args)	{ receiveArgs = _args; }
+		public void SendAsync(CustomeEventArgs _args)		{ sendArgs = _args; }
+
+		public void Startup()
+		{
+			Random _rand = new Random();
+			int _kind;
+			while (true)
+			{
+				_kind = _rand.Next() % 3;
+				switch (_kind)
+				{
+					case 0:
+						acceptArgs.OnComplete(this);
+						break;
+					case 1:
+						receiveArgs.OnComplete(this);
+						break;
+					case 2:
+						sendArgs.OnComplete(this);
+						break;
+				}
+
+				System.Threading.Thread.Sleep(100);
+			}
+		}
+	}
+
+
+	class Program
+	{
+		public CustomSocket socket = new CustomSocket();
+		void Startup()
+		{
+			CustomeEventArgs _args1 = new CustomeEventArgs("AcceptAsync");
+			_args1.Complete += OnAcceptCallback;
+			socket.AcceptAsync(_args1);
+
+			CustomeEventArgs _args2 = new CustomeEventArgs("ReceiveAsync");
+			_args2.Complete += OnReceiveCallback;
+			socket.ReceiveAsync(_args2);
+
+			CustomeEventArgs _args3 = new CustomeEventArgs("SendAsync");
+			_args3.Complete += OnSendCallback;
+			socket.SendAsync(_args3);
+
+			System.Threading.Thread _thread = new System.Threading.Thread(socket.Startup);
+			_thread.Start();
+		}
+
+
+		static void Main(string[] args)
+		{
+			Program _p = new Program();
+			_p.Startup();
+
+			while (true)
+			{
+				Console.WriteLine(".");
+				System.Threading.Thread.Sleep(1000);
+			}
+		}
+
+		void OnAcceptCallback(object _obj, CustomeEventArgs _acceptArgs){	Console.WriteLine(" > OnAcceptCallback:" + _acceptArgs.msg + " > " + _acceptArgs.callCount);}
+		void OnReceiveCallback(object _obj, CustomeEventArgs _receiveArgs){	Console.WriteLine(" > OnReceiveCallback:" + _receiveArgs.msg + " > " + _receiveArgs.callCount);}
+		void OnSendCallback(object _obj, CustomeEventArgs _sendArgs){		Console.WriteLine(" > OnSendCallback:" + _sendArgs.msg + " > " + _sendArgs.callCount);	}
+	}
+}
--- 09_CSharpTest/StackQueue/StackQueue/Program.cs
+++ 09_CSharpTest/StackQueue/StackQueue/Program.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 
 namespace StackQueue
 {
+	class CUserToken {
+		public byte[] buffer = new byte[1024];
+	}
+
 	class Program
 	{
 		public int LOOP = 1000000;
@@ -14,31 +18,68 @@ namespace StackQueue
 		static DateTime[] dt = new DateTime[10];
 		byte[] buffer = new byte[1024];
 		byte[] buffer2 = new byte[1024];
+		public Queue<CUserToken> freeUser = new Queue<CUserToken>();
 
+		void Init()
+		{
+			CUserToken _token;
+			for (int i = 0; i < 2000; i++)
+			{
+				_token = new CUserToken();
+				freeUser.Enqueue(_token);
+			}
+		}
 
 		static void Main(string[] args)
 		{
 			Program p = new Program();
+			p.Init();
+
 			dt[0] = DateTime.Now;
 			p.StackTest();
 			dt[1] = DateTime.Now;
 			p.QueueTest();
 			dt[2] = DateTime.Now;
-			p.StackTest();
-			dt[3] = DateTime.Now;
-			p.QueueTest();
-			dt[4] = DateTime.Now;
 			p.ClearBuffer();
-			dt[5] = DateTime.Now;
+			dt[3] = DateTime.Now;
 			p.CopyBuffer();
+			dt[4] = DateTime.Now;
+			p.CreateClass();
+			dt[5] = DateTime.Now;
+			p.UseQueueClass();
 			dt[6] = DateTime.Now;
+			dt[7] = DateTime.Now;
+			dt[8] = DateTime.Now;
+			dt[9] = DateTime.Now;
 
 			Console.WriteLine("StackTest:" + (int)((TimeSpan)(dt[1] - dt[0])).TotalMilliseconds);
-			Console.WriteLine("" + (int)((TimeSpan)(dt[2] - dt[1])).TotalMilliseconds);
-			Console.WriteLine((int)((TimeSpan)(dt[3] - dt[2])).TotalMilliseconds);
-			Console.WriteLine((int)((TimeSpan)(dt[4] - dt[3])).TotalMilliseconds);
-			Console.WriteLine((int)((TimeSpan)(dt[5] - dt[4])).TotalMilliseconds);
-			Console.WriteLine((int)((TimeSpan)(dt[6] - dt[5])).TotalMilliseconds);
+			Console.WriteLine("QueueTest:" + (int)((TimeSpan)(dt[2] - dt[1])).TotalMilliseconds);
+			Console.WriteLine("ClearBuffer:" + (int)((TimeSpan)(dt[3] - dt[2])).TotalMilliseconds);
+			Console.WriteLine("CopyBuffer:" + (int)((TimeSpan)(dt[4] - dt[3])).TotalMilliseconds);
+			Console.WriteLine("CreateClass:"+(int)((TimeSpan)(dt[5] - dt[4])).TotalMilliseconds);
+			Console.WriteLine("UseQueueClass:" + (int)((TimeSpan)(dt[6] - dt[5])).TotalMilliseconds);
+			Console.WriteLine("" + (int)((TimeSpan)(dt[7] - dt[6])).TotalMilliseconds);
+			Console.WriteLine("" + (int)((TimeSpan)(dt[8] - dt[7])).TotalMilliseconds);
+			Console.WriteLine("" + (int)((TimeSpan)(dt[9] - dt[8])).TotalMilliseconds);
+		}
+
+		public void UseQueueClass()
+		{
+			CUserToken _token;
+			for (int i = 0; i < LOOP; i++)
+			{
+				_token = freeUser.Dequeue();
+				freeUser.Enqueue(_token);
+			}
+		}
+
+		public void CreateClass()
+		{
+			CUserToken _token;
+			for (int i = 0; i < LOOP; i++)
+			{
+				_token = new CUserToken();
+			}
 		}
 
 		public void ClearBuffer()
@@ -49,8 +90,12 @@ namespace StackQueue
 
 		public void CopyBuffer()
 		{
+			int _len = buffer.Length;
+			for (int i = 0; i < _len; i++)
+				buffer2[i] = (byte)(i % 20);
+
 			for (int i = 0; i < LOOP; i++)
-				Array.Copy(buffer2, 0, buffer, 0, buffer.Length);
+				Array.Copy(buffer2, 0, buffer, 0, _len);
 		}
 
 		public void QueueTest()
@@ -92,7 +137,8 @@ namespace StackQueue
 						stack.Pop();
 					}
 				}
-			}			
+			}
 		}
 	}
+
 }
