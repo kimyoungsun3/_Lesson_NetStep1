8d23017e3d26bbb8767eb6660942f07068b647b6
kimyoungsun3 <40338139+kimyoungsun3@users.noreply.github.com>
2019-02-07 11:18:29 +0900

    2222
    2222

---
+++ 09_CSharpTest/EventArgs3/EventArgs3/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EventArgs3
+{
+	class CustomEventArgs : EventArgs
+	{
+		public string msg;
+		public CustomEventArgs(string _msg)
+		{
+			msg = _msg;
+		}
+	}
+
+	class Publisher
+	{
+		public event EventHandler<CustomEventArgs> Complete;
+
+		public void DoSomething()
+		{
+			if(Complete != null)
+			{
+				Complete(this, new CustomEventArgs("hi"));
+			}
+		}
+	}
+
+	class SubScriber
+	{
+		string id;
+		public SubScriber(string _id, Publisher _pub)
+		{
+			id = _id;
+			_pub.Complete += OnHandler;
+		}
+
+		void OnHandler(object _obj, CustomEventArgs _args)
+		{
+			Console.WriteLine(id + " > " + _args.msg);
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Publisher _pub = new Publisher();
+			SubScriber _sub1 = new SubScriber("sub1", _pub);
+			SubScriber _sub2 = new SubScriber("sub2", _pub);
+
+			_pub.DoSomething();
+		}
+	}
+}
---
+++ 09_CSharpTest/EventArgs4/EventArgs4/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EventArgs4
+{
+	class CustomEventArgs : EventArgs
+	{
+		public string msg;
+		public event EventHandler<CustomEventArgs> Complete;
+		public CustomEventArgs(string _msg)
+		{
+			msg = _msg;
+		}
+
+		public void CompleteJob(object _obj)
+		{
+			if(Complete != null)
+			{
+				Complete(_obj, this);
+			}
+		}
+	}
+
+	class Publisher
+	{
+		CustomEventArgs acceptArgs, receiveArgs, sendArgs;
+		public void AcceptAsync(CustomEventArgs _args) { acceptArgs = _args; }
+		public void ReceiveAsync(CustomEventArgs _args) { receiveArgs = _args; }
+		public void SendAsync(CustomEventArgs _args) { sendArgs = _args; }
+
+		public void Startup()
+		{
+			Random _rand = new Random();
+			int _kind;
+			while (true)
+			{
+				_kind = _rand.Next() % 3;
+				switch (_kind) 
+				{
+					case 0:
+						acceptArgs.CompleteJob(this);
+						break;
+					case 1:
+						receiveArgs.CompleteJob(this);
+						break;
+					case 2:
+						sendArgs.CompleteJob(this);
+						break;
+
+				}
+				System.Threading.Thread.Sleep(100);
+			}
+		}
+	}
+
+	class Program
+	{
+		Publisher pub = new Publisher();
+		static void Main(string[] args)
+		{
+
+			while (true)
+			{
+				Program _p = new Program();
+				_p.Startup();
+				while (true)
+				{
+					Console.WriteLine(".");
+					System.Threading.Thread.Sleep(1000);
+				}
+			}
+		}
+
+		void Startup()
+		{
+			CustomEventArgs _args1 = new CustomEventArgs("AcceptAsync");
+			_args1.Complete += OnAcceptCallback;
+			pub.AcceptAsync(_args1);
+
+			CustomEventArgs _args2 = new CustomEventArgs("ReceiveAsync");
+			_args2.Complete += OnReceiveCallback;
+			pub.ReceiveAsync(_args2);
+
+			CustomEventArgs _args3 = new CustomEventArgs("SendAsync");
+			_args3.Complete += OnSendCallback;
+			pub.SendAsync(_args3);
+
+			System.Threading.Thread _th = new System.Threading.Thread(pub.Startup);
+			_th.Start();
+
+		}
+
+		void OnAcceptCallback(object _obj, CustomEventArgs _args) { Console.WriteLine(_args.msg + " > "); }
+		void OnReceiveCallback(object _obj, CustomEventArgs _args) { Console.WriteLine(_args.msg + " > "); }
+		void OnSendCallback(object _obj, CustomEventArgs _args) { Console.WriteLine(_args.msg + " > "); }
+	}
+}
