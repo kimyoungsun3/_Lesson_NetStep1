08f3de5d4b80196ff32f6b87c21f829904b9103f
kimyoungsun3 <40338139+kimyoungsun3@users.noreply.github.com>
2019-02-22 22:22:54 +0900

    tttt
    ttttt

---
+++ 09_CSharpTest/100_AsyncRun/100_AsyncRun/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _100_AsyncRun
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			CountDownWrapper _a = new CountDownWrapper();
+			CountDownWrapper _b = new CountDownWrapper();
+			_a.CountDown(10);
+			_b.CountDown(10);
+			Console.WriteLine("---- Main 1 ----");
+			AutoResetEvent.WaitAll(new[] { _a.done, _b.done });
+			Console.WriteLine("---- Main 2 ----");
+		}
+	}
+
+	class CountDownWrapper
+	{
+		public AutoResetEvent done = new AutoResetEvent(false);
+		public void CountDown(int _cc)
+		{
+			Console.WriteLine(_cc--);
+			if(_cc >= 0)
+			{
+				Task.Delay(1000).ContinueWith((c) =>
+				{
+					CountDown(_cc);
+				});
+				Console.WriteLine(" ---->");
+			}
+			else
+			{
+				done.Set();
+			}
+			Console.WriteLine("----{0}----", _cc);
+		}
+	}
+}
---
+++ 09_CSharpTest/101_ForeachDelegate/101_ForeachDelegate/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks; //.NET 4.5
+
+namespace _101_ForeachDelegate
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Program _p = new Program();
+
+			_p.TestTask();
+		}
+		
+		void TestTask()
+		{
+			Console.WriteLine("Fun() -> 리스트에담아... foreach ..");
+			var heavyQuery = Enumerable.Range(0, 10).Where(c =>
+			{
+				Task.Delay(1000).Wait();
+				return true;
+			});
+
+			DateTime _start = DateTime.Now;
+			foreach (var _item in heavyQuery)
+			{
+				Console.Write(_item);
+			}
+			Console.WriteLine(" 소요시간:{0}", DateTime.Now - _start);
+		}
+	}
+}
