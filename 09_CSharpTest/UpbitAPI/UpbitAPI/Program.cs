740a81070327f4bea0f7d4b557b7fc59569b4345
kimyoungsun3 <40338139+kimyoungsun3@users.noreply.github.com>
2019-04-25 00:29:14 +0900

    333
    3333

--- 09_CSharpTest/UpbitAPI/UpbitAPI/Program.cs
+++ 09_CSharpTest/UpbitAPI/UpbitAPI/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.IdentityModel.Tokens.Jwt;
+//using System.IdentityModel.Tokens.Jwt;
 
 
 namespace UpbitAPI
@@ -17,23 +17,23 @@ namespace UpbitAPI
 		void UpbitAPI()
 		{
 
-			TimeSpan diff = DateTime.Now - new DateTime(1970, 1, 1);
-			var nonce = Convert.ToInt64(diff.TotalMilliseconds);
-			var payload = new JwtPayload
-			{
-				{ "access_key", "발급받은 Access Key" },
-				{ "nonce", nonce }
-			};
-			byte[] keyBytes = Encoding.Default.GetBytes("발급받은 Secret Key");
-			var securityKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(keyBytes);
-			var credentials = new Microsoft.IdentityModel.Tokens.SigningCredentials(securityKey, "HS256");
-			var header = new JwtHeader(credentials);
-			var secToken = new JwtSecurityToken(header, payload);
+			//TimeSpan diff = DateTime.Now - new DateTime(1970, 1, 1);
+			//var nonce = Convert.ToInt64(diff.TotalMilliseconds);
+			//var payload = new JwtPayload
+			//{
+			//	{ "access_key", "발급받은 Access Key" },
+			//	{ "nonce", nonce }
+			//};
+			//byte[] keyBytes = Encoding.Default.GetBytes("발급받은 Secret Key");
+			//var securityKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(keyBytes);
+			//var credentials = new Microsoft.IdentityModel.Tokens.SigningCredentials(securityKey, "HS256");
+			//var header = new JwtHeader(credentials);
+			//var secToken = new JwtSecurityToken(header, payload);
 
-			var jwtToken = new JwtSecurityTokenHandler().WriteToken(secToken);
-			var authorizationToken = "Bearer " + jwtToken;
+			//var jwtToken = new JwtSecurityTokenHandler().WriteToken(secToken);
+			//var authorizationToken = "Bearer " + jwtToken;
 
-			Console.WriteLine(authorizationToken);
+			//Console.WriteLine(authorizationToken);
 		}
 	}
 }
